28b7c778d99c2be9f8eec6e62d416d97a6e336a0
AdamEshem <adameshemwork@gmail.com>
2022-12-01 04:10:44 -0500

    Deadline drew near so here is basically the rest of it.

--- RSAFunctions.py
+++ RSAFunctions.py
@@ -34,3 +34,54 @@ def get_e(phi):
         e = int(input('Enter a value for e: '))
     
     return e
+
+def calculate_d(phi, e):
+    return pow(e, -1, phi)
+
+def breakup(m, blockSize):
+    blocks = ['']
+    counter = 0
+
+    for i in m:
+        if counter == blockSize:
+            counter = 0
+            blocks.append('')
+        blocks[len(blocks) - 1] += i
+        counter += 1
+    return blocks
+
+def encode(blocks):
+    encoded = []
+    count = 0
+    for i in blocks:
+        encoded.append(0)
+        for j in range(len(i)):
+            encoded[count] = encoded[count] | ord(i[j])
+            if j < len(i) - 1:
+                encoded[count] = encoded[count] << 8
+        count += 1
+    return encoded
+
+def encrypt(m, e, n):
+    for i in range(len(m)):
+        m[i] = pow(m[i], e, n)
+    return m
+
+def decrypt(ct, d, n):
+    for i in range(len(ct)):
+        ct[i] = pow(ct[i], d, n)
+    return ct
+
+def decode(m):
+    decoded = []
+    for i in range(len(m)):
+        decoded.append('')
+        while m[i]:
+            x = (m[i] & 0b11111111)
+            m[i] = m[i] >> 8
+            decoded[len(decoded) - 1] = chr(x) + decoded[len(decoded) - 1]
+    return decoded
+
+def listToString(list):
+    message = ''
+    return message.join(list)
