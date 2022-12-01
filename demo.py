b1c166ca1efcf7a42488fbb78e31f0e309487ce3
AdamEshem <adameshemwork@gmail.com>
2022-12-01 04:11:24 -0500

    A very basic working version of the demo without any of the bonus text.

--- demo.py
+++ demo.py
@@ -1,1 +1,30 @@
 from RSAFunctions import *
+
+
+primes = get_primes()
+
+n = calculate_N(primes)
+
+phi = calculate_phi(primes)
+
+e = get_e(phi)
+
+d = calculate_d(phi, e)
+
+print(d)
+
+blockSize = int.bit_length(n) // 8
+
+m = input('Enter secret message: ')
+
+blocks = breakup(m, blockSize)
+
+encoded = encode(blocks)
+
+encrypted = encrypt(encoded, e, n)
+
+decrypted = decrypt(encrypted, d, n)
+
+decoded = decode(decrypted)
+print(decoded)
+print(listToString(decoded))
